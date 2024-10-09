6cb2c81e424633cd6e917e79b7297de09e984a9c
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2024-10-09 11:34:47 +0300

    implementing student.cpp

--- student.cpp
+++ student.cpp
@@ -1,9 +1,11 @@
 #include "universityDB.h"
 
-void Student::inputData() {
+void Student::addStudentData() {
+    cout << "Enter student ID: ";
+    getline(cin, ID);
 
     cout << "Enter faculty abbreviation: ";
-    getline(cin, facultyAbbreviationStudentBelongs);
+    getline(cin, facultyAbbreviation);
 
     cout << "Enter first name: ";
     getline(cin, firstName);
@@ -25,11 +27,12 @@ void Student::inputData() {
     getline(cin, gradResponse);
 
     isGraduate = (gradResponse == "yes");
-    cout << "\nStudent added successfully!" << endl;
 }
 
-void Student::printData() const {
-    cout << "Faculty: " << facultyAbbreviationStudentBelongs
+void Student::displayStudentData() {
+    cout << endl;
+    cout << "\nID: " << ID
+         << "\nFaculty: " << facultyAbbreviation
          << "\nFirst name: " << firstName
          << "\nLast name: " << lastName
          << "\nEmail: " << email
@@ -38,27 +41,139 @@ void Student::printData() const {
          << endl;
 }
 
-void Student::saveToFile(ofstream& ofs) const {
-    ofs << facultyAbbreviationStudentBelongs << ","
-        << firstName << ","
-        << lastName << ","
-        << email << ","
-        << enrollmentDate << ","
-        << dateOfBirth << ","
-        << (isGraduate ? "yes" : "no") << endl;
+void Student::retrieveStudentByID() {
+    string uniqueID;
+    cout << "Enter ID: ";
+    getline(cin, uniqueID);
+
+    string enrolledStudents = "D:\\UTM\\OOP\\laboratories_TEST\\enrolledStudents.txt";
+    fstream file(enrolledStudents);
+    if (!file) {
+        cerr << "Failed to open the students file\n";
+        return;
+    }
+
+    string line;
+    bool found = false;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        Student student;
+
+        getline(ss, student.ID, '/');
+        getline(ss, student.facultyAbbreviation, '/');
+        getline(ss, student.lastName, '/');
+        getline(ss, student.firstName, '/');
+        getline(ss, student.email, '/');
+        getline(ss, student.enrollmentDate, '/');
+        getline(ss, student.dateOfBirth, '\n');
+
+        if (student.ID == uniqueID) {
+            cout << "Student found: \n"
+                 << "ID: " << student.ID << "\n"
+                 << "Faculty: " << student.facultyAbbreviation << "\n"
+                 << "Name: " << student.firstName << " " << student.lastName << "\n"
+                 << "Email: " << student.email << "\n"
+                 << "Enrollment Date: " << student.enrollmentDate << "\n"
+                 << "Date of Birth: " << student.dateOfBirth << "\n";
+            found = true;
+            break;
+        }
+    }
+    file.close();
+
+    if (!found) {
+        cout << "No student found" << endl;
+    }
 }
 
-void Student::loadFromFile(const string& line) {
-    stringstream ss(line);
-    getline(ss, facultyAbbreviationStudentBelongs, ',');
-    getline(ss, firstName, ',');
-    getline(ss, lastName, ',');
-    getline(ss, email, ',');
-    getline(ss, enrollmentDate, ',');
-    getline(ss, dateOfBirth, ',');
+void Student::deleteAStudentByID() {
+    string enrolledFilePath = "D:\\UTM\\OOP\\laboratories_TEST\\enrolledStudents.txt";
+    string graduatedFilePath = "D:\\UTM\\OOP\\laboratories_TEST\\graduatedStudents.txt";
+    string deleteID;
 
-    string gradResponse;
-    getline(ss, gradResponse, ',');
-    isGraduate = (gradResponse == "yes");
+    cout << "Enter the ID: ";
+    getline(cin, deleteID);
+
+    bool foundInEnrolled = false, foundInGraduated = false;
+
+    vector<string> enrolledStudents;
+    fstream enrolledFile(enrolledFilePath);
+    if (!enrolledFile) {
+        cerr << "Failed to open the file enrolled st" << endl;
+        return;
+    }
+
+    string line;
+    while (
+            getline(enrolledFile, line)) {
+        stringstream ss(line);
+        string currentID;
+        getline(ss, currentID, '/');
+
+        if (currentID != deleteID) {
+            enrolledStudents.
+                    push_back(line);
+        } else {
+            foundInEnrolled = true;
+        }
+    }
+    enrolledFile.close();
+
+    ofstream outEnrolledFile(enrolledFilePath);
+    if (!outEnrolledFile) {
+        cerr << "Failed to open the file enrolled st" << endl;
+        return;
+    }
+
+    for (size_t i = 0; i < enrolledStudents.size(); ++i) {
+        outEnrolledFile << enrolledStudents[i];
+        if (i < enrolledStudents.size() - 1) {
+            outEnrolledFile << '\n';
+        }
+    }
+    outEnrolledFile.close();
+
+    vector<string> graduatedStudents;
+    fstream graduatedFile(graduatedFilePath);
+    if (!graduatedFile) {
+        cerr << "Failed to open the file" << endl;
+        return;
+    }
+
+    while (getline(graduatedFile, line)) {
+        stringstream ss(line);
+        string currentID;
+        getline(ss, currentID, '/');
+
+        if (currentID != deleteID) {
+            graduatedStudents.
+                    push_back(line);
+        } else {
+            foundInGraduated = true;
+        }
+    }
+    graduatedFile.close();
+
+    ofstream outGraduatedFile(graduatedFilePath);
+    if (!outGraduatedFile) {
+        cerr << "Failed to open the file graduated st" << endl;
+        return;
+    }
+
+    for (size_t i = 0; i < graduatedStudents.size(); ++i) {
+        outGraduatedFile << graduatedStudents[i];
+        if (i < graduatedStudents.size() - 1) {
+            outGraduatedFile << '\n';
+        }
+    }
+    outGraduatedFile.close();
+
+    if (foundInEnrolled) {
+        cout << "Student with ID " << deleteID << " has been deleted successfully from enrolled students!\n";
+    } else if (foundInGraduated) {
+        cout << "Student with ID " << deleteID << " has been deleted successfully from graduated students!\n";
+    } else {
+        cout << "No student found" << "\n";
+    }
 }
-
