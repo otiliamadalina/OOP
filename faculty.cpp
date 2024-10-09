b12186592baeebc7f43c4620b7633af999f5f806
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2024-10-09 11:37:35 +0300

    implement faculty.cpp

--- faculty.cpp
+++ faculty.cpp
@@ -1,199 +1,357 @@
 #include "universityDB.h"
 
 void Faculty::createFaculty() {
-    FacultyInfo newFaculty;
 
     cout << "Enter faculty name: ";
-    getline(cin, newFaculty.facultyName);
+    getline(cin, facultyName);
 
     cout << "Enter faculty abbreviation: ";
-    getline(cin, newFaculty.facultyAbbreviation);
+    getline(cin, facultyAbbreviation);
 
-    cout << "\nSelect study field:\n";
-    for (int i = 0; i < FIELD_COUNT; ++i) {
-        cout << i + 1 << ". " << fieldNames[i] << endl;
-    }
-    int fieldChoice;
-    cout << "Enter choice (1-" << FIELD_COUNT << "): ";
-    cin >> fieldChoice;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Enter study field:";
+    getline(cin, studyField);
 
-    if (fieldChoice >= 1 && fieldChoice <= FIELD_COUNT) {
-        newFaculty.studyField = static_cast<StudyField>(fieldChoice - 1);
-        faculties.push_back(newFaculty);
-        cout << "Faculty created successfully!\n";
-    } else {
-        cout << "Invalid choice. Faculty not created.\n";
-    }
+    cout << "\nFaculty created successfully!" << endl;
 }
 
-void Faculty::displayFaculties() {
-    cout << "\n" << endl;
-    for (const auto &faculty: faculties) {
-        cout << "Faculty Name: " << faculty.facultyName << endl;
-        cout << "Abbreviation: " << faculty.facultyAbbreviation << endl;
-        cout << "Study Field: " << fieldNames[faculty.studyField] << endl;
-    }
+void Faculty::displayAllFaculties() {
+    cout << endl;
+    cout << "\nFaculty Name: " << facultyName;
+    cout << "\nAbbreviation: " << facultyAbbreviation;
+    cout << "\nStudy Field: " << studyField << endl;
+
 }
 
 void Faculty::displayAllFacultiesOfAField() {
-    cout << "Select study field:\n";
-    for (int i = 0; i < FIELD_COUNT; ++i) {
-        cout << i + 1 << ". " << fieldNames[i] << endl;
+    string searchField;
+
+    cout << "Enter the study field to search for faculties: ";
+    getline(cin, searchField);
+
+    cout << "\nFaculties for the field " << searchField << "\":\n";
+
+    string TUMfaculties = "D:\\UTM\\OOP\\laboratories_TEST\\TUMfaculties.txt";
+    fstream file(TUMfaculties);
+    if (!file) {
+        cerr << "Failed to open the file!\n";
+        return;
     }
 
-    int fieldChoice;
-    cout << "Enter choice (1-" << FIELD_COUNT << "): ";
-    cin >> fieldChoice;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    string line;
+    bool found = false;
 
-    if (fieldChoice >= 1 && fieldChoice <= FIELD_COUNT) {
-        auto selectedField = static_cast<StudyField>(fieldChoice - 1);
-        cout << "Faculties in the field of " << fieldNames[selectedField] << ":\n";
+    while (getline(file, line)) {
+        stringstream ss(line);
+        Faculty faculty;
 
-        bool found = false;
-        for (const auto &faculty: faculties) {
-            if (faculty.studyField == selectedField) {
-                cout << "Faculty Name: " << faculty.facultyName
-                     << ", Abbreviation: " << faculty.facultyAbbreviation << endl;
-                found = true;
-            }
-        }
-        if (!found) {
-            cout << "No faculties found for the selected field." << endl;
-        }
-    } else {
-        cout << "Invalid choice.\n";
-    }
-}
+        getline(ss, faculty.facultyAbbreviation, '/');
+        getline(ss, faculty.facultyName, '/');
+        getline(ss, faculty.studyField, '/');
 
-void Faculty::graduateStudentByEmail(const string &email) {
-    cout << "List of graduated students: " << endl;
-    for (int i = 0; i < nr_of_students; i++) {
-        if (list_of_students[i].getEmail() == email) {
-            list_of_students[i].setGraduationStatus(true);
-            cout << "Student with email " << email << " has been marked as graduated.\n";
-            return;
+        if (faculty.studyField == searchField) {
+            faculty.displayAllFaculties();
+            found = true;
         }
     }
-    cout << "No student found with the email: " << email << ".\n";
+
+    if (!found) {
+        cout << "No faculties found.\n";
+    }
+    file.close();
 }
 
-void Faculty::addStudentData() {
-    if (nr_of_students < 500) {
-        list_of_students[nr_of_students].inputData();
-        nr_of_students++;
-    } else {
-        cout << "Maximum number of students reached." << endl;
-    }
-}
-
-void Faculty::displayStudents() const {
-    string facultyAbb;
-    cout << "Enter faculty abbreviation to display enrolled students: ";
-    getline(cin, facultyAbb);
-
-    cout << "List of students in faculty " << facultyAbb << ": " << endl;
-    bool foundEnrolled = false;
-
-    for (int i = 0; i < nr_of_students; i++) {
-
-        if (!list_of_students[i].isGraduated() &&
-            list_of_students[i].facultyAbbreviationStudentBelongs == facultyAbb) {
-            list_of_students[i].printData();
-            foundEnrolled = true;
-        }
-    }
-    if (!foundEnrolled) {
-        cout << "No enrolled students found for faculty: " << facultyAbb << "." << endl;
-    }
-}
-
-void Faculty::displayGraduatedStudents() const {
-    string facultyAbb;
-    cout << "Enter faculty abbreviation to display graduated students: ";
-    getline(cin, facultyAbb);
-
-    cout << "List of graduated students in faculty " << facultyAbb << ": " << endl;
-    bool foundGraduated = false;
-
-    cout << endl;
-    for (int i = 0; i < nr_of_students; i++) {
-
-        if (list_of_students[i].isGraduated() &&
-            list_of_students[i].facultyAbbreviationStudentBelongs == facultyAbb) {
-            list_of_students[i].printData();
-            foundGraduated = true;
-        }
-    }
-    if (!foundGraduated) {
-        cout << "No graduated students found for faculty " << facultyAbb << "." << endl;
-    }
-}
-
-void Faculty::searchStudent() const {
-
+void Faculty::graduateAStudentByEmail() {
     string email;
-    cout << "Enter email of the student to search: ";
+    cout << "Enter the email: ";
     getline(cin, email);
 
+    string enrolledStudents = "D:\\UTM\\OOP\\laboratories_TEST\\enrolledStudents.txt";
+    string graduatedStudents ="D:\\UTM\\OOP\\laboratories_TEST\\graduatedStudents.txt";
+    fstream file(enrolledStudents);
+    if (!file) {
+        cerr << "Failed to open the file: " << enrolledStudents << "!\n";
+        return;
+    }
+
+    string line;
     bool found = false;
-    for (int i = 0; i < nr_of_students; i++) {
-        if (list_of_students[i].getEmail() == email) {
-            list_of_students[i].printData();
-            cout << "Faculty: ";
-            for (const auto &faculty: faculties) {
-                if (faculty.facultyAbbreviation == list_of_students[i].facultyAbbreviationStudentBelongs) {
-                    cout << faculty.facultyName << " (" << faculty.facultyAbbreviation << ")\n";
-                    break;
-                }
+
+    vector<Student> students;
+    Student graduatedStudent;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        Student student;
+
+        getline(ss, student.ID, '/');
+        getline(ss, student.facultyAbbreviation, '/');
+        getline(ss, student.lastName, '/');
+        getline(ss, student.firstName, '/');
+        getline(ss, student.email, '/');
+        getline(ss, student.enrollmentDate, '/');
+        getline(ss, student.dateOfBirth, '\n');
+
+        if (student.getEmail() == email) {
+            graduatedStudent = student;
+            found = true;
+            cout << "Student graduated successfully!" << endl;
+        } else {
+            students.push_back(student);
+        }
+    }
+    file.close();
+
+    ofstream outFile(enrolledStudents);
+    if (!outFile) {
+        cerr << "Failed to open the file enrolledStudents" << "\n";
+        return;
+    }
+
+    for (size_t i = 0; i < students.size(); ++i) {
+        const auto& student = students[i];
+        outFile << student.ID << '/'
+                << student.facultyAbbreviation << '/'
+                << student.lastName << '/'
+                << student.firstName << '/'
+                << student.email << '/'
+                << student.enrollmentDate << '/'
+                << student.dateOfBirth;
+
+        if (i < students.size() - 1) {
+            outFile << '\n';
+        }
+    }
+
+    outFile.close();
+
+    if (found) {
+        ofstream gradFile(graduatedStudents, ios::app);
+        if (!gradFile) {
+            cerr << "Failed to open the file graduated st" << "\n";
+            return;
+        }
+
+        if (file.tellp() != 0) {
+            file << '\n';
+        }
+
+        gradFile << graduatedStudent.ID << '/'
+                 << graduatedStudent.facultyAbbreviation << '/'
+                 << graduatedStudent.lastName << '/'
+                 << graduatedStudent.firstName << '/'
+                 << graduatedStudent.email << '/'
+                 << graduatedStudent.enrollmentDate << '/'
+                 << graduatedStudent.dateOfBirth << '\n';
+
+        gradFile.close();
+
+    } else {
+        cout << "No student found" << endl;
+    }
+}
+
+void Faculty::displayEnrolledStudents() {
+    string facultyAbb;
+    cout << "Enter faculty abbreviation ";
+    getline(cin, facultyAbb);
+
+    cout << "List of enrolled students in faculty " << facultyAbb << ": " << endl;
+    cout << "\n";
+
+    string enrolledStudenets = "D:\\UTM\\OOP\\laboratories_TEST\\enrolledStudents.txt";
+    fstream file(enrolledStudenets);
+
+    if (!file) {
+        cerr << "Failed to open the students file\n";
+        return;
+    }
+
+    string line;
+    bool found = false;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        Student student;
+
+        getline(ss, student.ID, '/');
+        getline(ss, student.facultyAbbreviation, '/');
+        getline(ss, student.lastName, '/');
+        getline(ss, student.firstName, '/');
+        getline(ss, student.email, '/');
+        getline(ss, student.enrollmentDate, '/');
+        getline(ss, student.dateOfBirth, '/');
+
+        if (student.facultyAbbreviation == facultyAbb) {
+            cout << "ID: " << student.ID << endl;
+            cout << "Faculty Abbreviation: " << student.facultyAbbreviation << endl;
+            cout << "First Name: " << student.firstName << endl;
+            cout << "Last Name: " << student.lastName << endl;
+            cout << "Email: " << student.email << endl;
+            cout << "Enrollment Date: " << student.enrollmentDate << endl;
+            cout << "Date of Birth: " << student.dateOfBirth << endl;
+            cout << "--------------------------\n";
+            found = true;
+        }
+    }
+    if (!found) {
+        cout << "No enrolled students found" << endl;
+    }
+    file.close();
+}
+
+void Faculty::displayGraduatedStudents(){
+    cout << "\nEnter the faculty abbreviation: ";
+    string facultyAbb;
+    getline(cin, facultyAbb);
+
+    cout << "\nList of Graduated Students from " << facultyAbb << ":\n";
+
+    string graduatedStudents = "D:\\UTM\\OOP\\laboratories_TEST\\graduatedStudents.txt";
+    fstream file(graduatedStudents);
+    if (!file) {
+        cerr << "Failed to open the file!\n";
+        return;
+    }
+
+    string line;
+    bool found = false;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        Student student;
+
+        getline(ss, student.ID, '/');
+        getline(ss, student.facultyAbbreviation, '/');
+        getline(ss, student.lastName, '/');
+        getline(ss, student.firstName, '/');
+        getline(ss, student.email, '/');
+        getline(ss, student.enrollmentDate, '/');
+        getline(ss, student.dateOfBirth, '/');
+        string graduationStatus;
+        getline(ss, graduationStatus, '/');
+
+        if (student.facultyAbbreviation == facultyAbb) {
+            cout << student.getFirstName() << " " << student.getLastName() << " (" << student.email << ")\n";
+            found = true;
+        }
+    }
+    if (!found) {
+        cout << "No graduated students found" << endl;
+    }
+    file.close();
+}
+
+void Faculty::searchStudentByEmail() {
+    string email;
+    cout << "Enter the email: ";
+    getline(cin, email);
+
+    string enrolledStudents = "D:\\UTM\\OOP\\laboratories_TEST\\enrolledStudents.txt";
+    fstream file(enrolledStudents);
+    if (!file) {
+        cerr << "Failed to open the file!\n";
+        return;
+    }
+
+    cout << "Enrolled students";
+
+    string line;
+    bool found = false;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        Student student;
+
+        getline(ss, student.ID, '/');
+        getline(ss, student.facultyAbbreviation, '/');
+        getline(ss, student.lastName, '/');
+        getline(ss, student.firstName, '/');
+        getline(ss, student.email, '/');
+        getline(ss, student.enrollmentDate, '/');
+        getline(ss, student.dateOfBirth, '/');
+
+        if (student.getEmail() == email) {
+            student.displayStudentData();
+            found = true;
+            break;
+        }
+    }
+    file.close();
+
+    if (!found) {
+        string graduatedStudents = "D:\\UTM\\OOP\\laboratories_TEST\\graduatedStudents.txt";
+        fstream gradFile(graduatedStudents);
+        if (!gradFile) {
+            cerr << "Failed to open the file!" << endl;
+            return;
+        }
+
+        while (getline(gradFile, line)) {
+            stringstream ss(line);
+            Student student;
+
+            getline(ss, student.ID, '/');
+            getline(ss, student.facultyAbbreviation, '/');
+            getline(ss, student.lastName, '/');
+            getline(ss, student.firstName, '/');
+            getline(ss, student.email, '/');
+            getline(ss, student.enrollmentDate, '/');
+            getline(ss, student.dateOfBirth, '/');
+
+            if (student.getEmail() == email) {
+                cout << "Graduated student:";
+                student.displayStudentData();
+                found = true;
+                break;
             }
+        }
+        gradFile.close();
+    }
+    if (!found) {
+        cout << "No student found" << endl;
+    }
+}
+
+void Faculty::checkStudentBelongsFaculty() {
+    string email, facultyAbb;
+    cout << "Enter the faculty abbreviation: ";
+    getline(cin, facultyAbb);
+
+    cout << "Enter the email: ";
+    getline(cin, email);
+
+    cout << "\nStudent with email: " << email;
+
+    string enrolledStudents = "D:\\UTM\\OOP\\laboratories_TEST\\enrolledStudents.txt";
+    fstream file(enrolledStudents);
+    if (!file) {
+        cerr << "Failed to open the file!\n";
+        return;
+    }
+
+    string line;
+    bool found = false;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        Student student;
+
+        getline(ss, student.ID, '/');
+        getline(ss, student.facultyAbbreviation, '/');
+        getline(ss, student.lastName, '/');
+        getline(ss, student.firstName, '/');
+        getline(ss, student.email, '/');
+        getline(ss, student.enrollmentDate, '/');
+        getline(ss, student.dateOfBirth, '/');
+
+        if (student.getEmail() == email && student.facultyAbbreviation == facultyAbb) {
+            student.displayStudentData();
             found = true;
             break;
         }
     }
     if (!found) {
-        cout << "No student found with the email: " << email << endl;
+        cout << "No student found" << endl;
     }
-}
-
-void Faculty::checkStudentBelongsFaculty(const string& facultyAbbreviation, const string& studentEmail) {
-
-    for (int i = 0; i < nr_of_students; i++) {
-        const auto& student = list_of_students[i];
-        if (student.getEmail() == studentEmail) {
-            if (student.isGraduated()) {
-                cout << "Student " << student.getFirstName() << " " << student.getLastName()
-                          << " graduated from the " << facultyAbbreviation << "Faculty." << endl;
-            } else {
-                cout << "Student " << student.getFirstName() << " " << student.getLastName()
-                          << " belongs to " << facultyAbbreviation << "Faculty.\n";
-            }
-            return;
-        }
-    }
-    cout << "Student with email " << studentEmail << " doesnt belong to any faculty" << facultyAbbreviation << ".\n";
-}
-
-void Faculty::saveToFile(ofstream &ofs) const {
-    ofs << facultyName << "," << facultyAbbreviation << endl;
-    for (int i = 0; i < nr_of_students; i++) {
-        list_of_students[i].saveToFile(ofs);
-    }
-}
-
-void Faculty::loadFromFile(const string &line) {
-    stringstream ss(line);
-    getline(ss, facultyName, ',');
-    getline(ss, facultyAbbreviation, ',');
-
-}
-
-void Faculty::addStudent(const Student& student) {
-    if (nr_of_students < 500) {
-        list_of_students[nr_of_students] = student;
-        nr_of_students++;
-    } else {
-        cout << "Maximum number of students reached." << endl;
-    }
+    file.close();
 }
